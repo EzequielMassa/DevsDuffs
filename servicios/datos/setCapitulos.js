f5dee593a6dd37ea44cfec1e9c6a3b65f251a0a8
georginacostilla <georginacostilla.gc@gmail.com>
2024-01-24 15:31:31 -0300

    se modifica nombre de una const

--- servicios/datos/setCapitulos.js
+++ servicios/datos/setCapitulos.js
@@ -1,8 +1,8 @@
-import capitulosDb from "../datos/capitulos.json" assert { type: "json" };
+import capitulosDato from "../datos/capitulos.json" assert { type: "json" };
 
 export const setcapitulos = ()=> {
     const capitulos = JSON.parse(localStorage.getItem("capitulos")) || null
     if (!capitulos || capitulos?.length == 0) {
-        localStorage.setItem("capitulos", JSON.stringify(capitulosDb))
+        localStorage.setItem("capitulos", JSON.stringify(capitulosDato))
     }
 }
