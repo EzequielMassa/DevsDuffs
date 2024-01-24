e1129498b510e1c173f966883ef43b5c0938825a
georginacostilla <georginacostilla.gc@gmail.com>
2024-01-24 15:29:48 -0300

    se modifica nombre de una constante

--- servicios/datos/setCapitulos.js
+++ servicios/datos/setCapitulos.js
@@ -1,8 +1,8 @@
-import capitulos from "../datos/capitulos.json" assert { type: "json" };
+import capitulosDb from "../datos/capitulos.json" assert { type: "json" };
 
 export const setcapitulos = ()=> {
     const capitulos = JSON.parse(localStorage.getItem("capitulos")) || null
-    if (!capitulos || capitulos?.lngth == 0) {
-        localStorage.setItem("capitulos", JSON.stringify(capitulos))
+    if (!capitulos || capitulos?.length == 0) {
+        localStorage.setItem("capitulos", JSON.stringify(capitulosDb))
     }
 }
