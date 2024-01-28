42f16d92af0937fc344995b4d139225363a6f43a
georginacostilla <georginacostilla.gc@gmail.com>
2024-01-28 18:37:13 -0300

    se agrega funcion que redirecciona al error 404

--- paginas/paginaPrincipalInferior/funcionalidadBoton.js
+++ paginas/paginaPrincipalInferior/funcionalidadBoton.js
@@ -1,6 +1,12 @@
 let miBoton = document.getElementById('miBoton');
+let boton404 = document.getElementById('boton404');
+
 
 miBoton.addEventListener('click', function() {
+    window.location.href = '/paginas/detalleDeCategoria/detalleDeCategoria.html';
+});
 
-    window.location.href = '';
+
+boton404.addEventListener('click', function() {
+    window.location.href = ''; //colocar la ruta del 404 
 });
