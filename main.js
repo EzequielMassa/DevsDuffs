1fb27f4cc81d1c984feabcc88f9cc21199ea0314
georginacostilla <georginacostilla.gc@gmail.com>
2024-01-28 21:20:00 -0300

    se elimina console.log de prueba

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
 // import { v4 as uuidv4 } from 'https://jspm.dev/uuid'
 import {setcapitulos} from "./servicios/datos/setCapitulos.js"
-
 setcapitulos();
--- servicios/datos/setCapitulos.js
+++ servicios/datos/setCapitulos.js
@@ -2,7 +2,6 @@ import capitulosDatos from "../../datos/capitulos.json" assert { type: "json" };
 
 export const setcapitulos = ()=> {
     const capitulos = JSON.parse(localStorage.getItem("capitulos")) || null
-    console.log(capitulos)
     if (!capitulos || capitulos?.length == 0) {
         localStorage.setItem("capitulos", JSON.stringify(capitulosDatos))
     }
