cf751a4e4d3177e0a55776d4c3a45e47b79bb710
georginacostilla <georginacostilla.gc@gmail.com>
2024-01-31 16:59:50 -0300

    se agregan las funciones de los botones a main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 // import { v4 as uuidv4 } from 'https://jspm.dev/uuid'
+
+let miBoton = document.getElementById('miBoton');
+let boton404 = document.getElementsByClassName('boton404');
+
+
+miBoton.addEventListener('click', function() {
+    window.location.href = '/paginas/detalleDeCategoria/detalleDeCategoria.html';
+});
+
+boton404.addEventListener('click', function() {
+    window.location.href = '/paginas/error404/error404.html';
+});
