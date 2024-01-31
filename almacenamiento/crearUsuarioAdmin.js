d55c27a22d0770806ca66fc1029b43d3f84ef65a
Ezequiel Massa <ezequielmassa.dev@gmail.com>
2024-01-31 18:55:51 -0300

    el usuario admin ahora se crea automaticamente

---
+++ almacenamiento/crearUsuarioAdmin.js
@@ -0,0 +1,22 @@
+import { obtenerUsuarios } from '../almacenamiento/obtenerUsuarios.js'
+import { Usuario } from '../modelos/Usuario.js'
+import { guardarUsuario } from './guardarUsuario.js'
+
+/**
+ * Crea un usuario Admin si no existe en la lista de usuarios y lo guarda en el local storage.
+ *
+ * @return {void} - No retorna nada
+ */
+export const crearUsuarioAdmin = () => {
+	const usuarios = obtenerUsuarios()
+	const existeAdmin = usuarios.find(
+		(usuario) => usuario.email === 'admin@devsduff.com'
+	)
+	if (!existeAdmin) {
+		const admin = new Usuario('admin@devsduff.com', 'admin', 'admin')
+		admin.estadoUsuario.pendiente = false
+		admin.estadoUsuario.aprobado = true
+		guardarUsuario(admin)
+		return
+	}
+}
