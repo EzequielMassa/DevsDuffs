8ce2f8dff8189deaf9b4f07053f28aeae8a8d8bd
Ezequiel Massa <ezequielmassa.dev@gmail.com>
2024-02-03 18:19:34 -0300

    refactorizado error404.html

--- paginas/detalleDeCategoria/contenidoDetalleTemp.js
+++ paginas/detalleDeCategoria/contenidoDetalleTemp.js
@@ -1,5 +1,5 @@
-import { renderizarNavbar } from '../../componentes/navbar/navbar.js'
 import { renderizarCapitulos } from '../../componentes/capitulos/renderizarCapitulos.js'
+import { renderizarNavbar } from '../../componentes/navbar/navbar.js'
 
 renderizarNavbar()
 renderizarCapitulos()
