94463be7aba48c9a3100e1c9cbc602524bb7b36f
Ezequiel Massa <ezequielmassa.dev@gmail.com>
2024-02-03 18:51:48 -0300

    refactorizado validarNombre.js

--- validadores/validarNombre.js
+++ validadores/validarNombre.js
@@ -1,20 +1,16 @@
-import { mostrarInputError } from "../utilidades/mostrarInputError.js";
-import { limpiarInputError } from "../utilidades/limpiarInputError.js";
+import { limpiarInputError } from '../utilidades/limpiarInputError.js'
+import { mostrarInputError } from '../utilidades/mostrarInputError.js'
 
 /* Validacion un nombre en un formulario. Retorna true si es un formato valido , false si no lo es.*/
 
-
 export const validarNombre = (nombreInput) => {
-const nombreValor = nombreInput.value
+	const nombreValor = nombreInput.value
 
-if (!nombreValor || nombreValor.lenght > 10){
-    mostrarInputError(nombreInput);
-    return false
-}else{
-    limpiarInputError(nombreInput);
-    return true
+	if (!nombreValor || nombreValor.lenght > 10) {
+		mostrarInputError(nombreInput)
+		return false
+	} else {
+		limpiarInputError(nombreInput)
+		return true
+	}
 }
-
-
-}
-//este es
